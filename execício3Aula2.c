b2efb9b412a6ad236ec2ffca1d5a021ea240960e
lucasrocato <88353882+rocatonachoz@users.noreply.github.com>
2024-08-21 19:51:18 +0000

    first commit

--- execício3Aula2.c
+++ execício3Aula2.c
@@ -2,5 +2,17 @@
 
 int main()
 {
+    //Atribuindo variáveis e definindo input 
+    float temp, result;
     
+    printf("Insira um valor de graus Celcius para converter para Fahrenheit: ");
+    scanf("%f", &temp);
+
+    //Realizando cálculo 
+    result = (9/5 * temp) + 32;
+
+    //Imprimindo resultados
+    printf("Uma temperatura de %.2f °C equivale a %.2f °F.", temp, result);
+
+    return 0;
 }
